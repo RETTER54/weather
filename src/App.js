c874672b0510678744a9f8ee4a919b4cf3eca817
JRetter <jamila.retter54@gmail.com>
2020-02-18 15:30:56 +0100

    commit ajout axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather.js'
@@ -7,18 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <img src="soleil.png" className="App-logo" alt="logo" />
+        
         <Weather/>
       </header>
     </div>
